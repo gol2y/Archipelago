bd47c902faa465b65dcce3cd82146ff132fcd23b
Miruku <gol2y.st@gmail.com>
2017-04-22 00:04:39 +0700

    [refactor] remove duplicate new Object

--- test/th/co/scb/quest/ArchipelagoTest.java
+++ test/th/co/scb/quest/ArchipelagoTest.java
@@ -6,23 +6,22 @@ import org.junit.Test;
 
 public class ArchipelagoTest {
 
+	private Archipelago archipelago = new Archipelago();
+	
 	@Test
 	public void when_create_10_island_has_correct_size_island() {
-		Archipelago archipelago = new Archipelago();
 		archipelago.createIslands(10);
 		assertEquals(10, archipelago.getIslands().length);
 	}
 	
 	@Test
 	public void when_create_21_island_has_correct_size_island() {
-		Archipelago archipelago = new Archipelago();
 		archipelago.createIslands(21);
 		assertEquals(21, archipelago.getIslands().length);
 	}
 	
 	@Test
 	public void when_set_day_choose_have_correct_day() {
-		Archipelago archipelago = new Archipelago();
 		archipelago.setDay(15);
 		assertEquals(15, archipelago.getDay());
 	}
