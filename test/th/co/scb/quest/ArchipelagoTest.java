d810e8dc4d30ff7f0c1d660737d3b3f5aa88d474
Miruku <gol2y.st@gmail.com>
2017-04-21 23:59:18 +0700

    add new create island case

--- test/th/co/scb/quest/ArchipelagoTest.java
+++ test/th/co/scb/quest/ArchipelagoTest.java
@@ -7,10 +7,17 @@ import org.junit.Test;
 public class ArchipelagoTest {
 
 	@Test
-	public void when_create_island_has_correct_size_island() {
+	public void when_create_10_island_has_correct_size_island() {
 		Archipelago archipelago = new Archipelago();
 		archipelago.createIslands(10);
 		assertEquals(10, archipelago.getIslands().length);
+	}
+	
+	@Test
+	public void when_create_21_island_has_correct_size_island() {
+		Archipelago archipelago = new Archipelago();
+		archipelago.createIslands(21);
+		assertEquals(21, archipelago.getIslands().length);
 	}
 
 }
